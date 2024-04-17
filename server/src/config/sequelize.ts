cb0f0002c5ad281527cf8d99102c663c5306424b
Viper292 <crazymelon@hotmail.es>
2024-04-17 15:08:39 -0300

    Adds database configuration

---
+++ server/src/config/sequelize.ts
@@ -0,0 +1,34 @@
+const { Sequelize } = require("sequelize-typescript");
+const path = require('path');
+const dbConfig = require("./dbConfig");
+
+const { database, username, password, host, port, dialect } = dbConfig[process.env.NODE_ENV];
+
+const sequelize = new Sequelize({
+  database,
+  username,
+  password,
+  host,
+  port,
+  dialect,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: process.env.NODE_ENV === 'development' ? false : true
+    }
+  }
+  // models: [path.resolve(__dirname, '../src/models')],
+});
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection established successfully!');
+  } catch(err) {
+    console.log('Connection failed!:', err)
+  }
+}
+
+testConnection();
+
+export default sequelize;
