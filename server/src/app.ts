31bbc715e107e0086f2ef356675ad93704dd2422
Viper292 <crazymelon@hotmail.es>
2024-04-18 17:33:03 -0300

    Adds sequelize model sync

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import routerApi from './router/index';
+import sequelize from './config/sequelize';
 
 const PORT = parseInt(process.env.PORT as string, 10) || 3000;
 
@@ -12,6 +13,15 @@ app.get('/', (req, res) => {
     res.send({ data: 'Hello World' });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server is running on port https://localhost:${PORT}`);
+    try {
+        await sequelize.sync(
+            //NODE_ENV === 'production' ? { alter: true } : { force: true },
+            { alter: true },
+        )
+        console.log('Connection has been established successfully.')
+    } catch (error) {
+        console.error('Unable to connect to the database:', error)
+    }
 });
