642af9dc59cd8c63ae7bd3208511836fac5560d8
Viper292 <crazymelon@hotmail.es>
2024-04-19 16:26:12 -0300

    Adds admin creation route

--- server/src/router/users.router.ts
+++ server/src/router/users.router.ts
@@ -7,6 +7,8 @@ const router: Router = express.Router();
 router.get('/', UserController.getAllUsers);
 // GET /users/:id
 router.get('/:id', UserController.getUserById);
+// POST /users/admin/:id
+router.post('/admin/:id', UserController.createAdminUser);
 // PATCH /users/:id
 router.patch('/:id', UserController.updateUser);
 // DELETE /users/:id
