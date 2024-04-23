54e8682339a137408edc9537f7be37d401083319
Viper292 <crazymelon@hotmail.es>
2024-04-23 03:15:20 -0300

    Creates basic categories module

---
+++ server/src/controllers/categories.controller.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+class CatsController {
+
+}
+
+export default CatsController;
---
+++ server/src/router/categories.router.ts
@@ -0,0 +1,6 @@
+import express, { Router } from 'express';
+import CatsController from '../controllers/categories.controller';
+
+const router: Router = express.Router();
+
+export default router;
