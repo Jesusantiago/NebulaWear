046ce5ce9d5c3d00d008d0f875381a89a255e959
Viper292 <crazymelon@hotmail.es>
2024-04-23 04:55:09 -0300

    Adds POST functionality

--- server/src/controllers/category.controller.ts
+++ server/src/controllers/category.controller.ts
@@ -1,7 +1,30 @@
 import { Request, Response } from "express";
+import Category from "../models/category.model";
 
 class CatController {
+  static async createCategory(req: Request, res: Response) {
+    try {
+      const { name, is_featured } = req.body;
+      const category = await Category.findOne({
+        where: {
+          name: name
+        }
+      })
 
+      if (category) {
+        return res.status(409).json({ message: "La categoría ingresada ya existe." });
+      }
+
+      const newCategory = await Category.create({
+        name: name,
+        is_featured: is_featured
+      })
+
+      res.status(201).json({ message: 'Categoría creada con éxito.', newCategory });
+    } catch(err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
 }
 
 export default CatController;
