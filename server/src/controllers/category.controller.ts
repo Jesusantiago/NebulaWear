017425946911d580e4fcb4132229c4980a03f5ac
Viper292 <crazymelon@hotmail.es>
2024-04-23 05:05:39 -0300

    Adds GET functionality

--- server/src/controllers/category.controller.ts
+++ server/src/controllers/category.controller.ts
@@ -2,6 +2,29 @@ import { Request, Response } from "express";
 import Category from "../models/category.model";
 
 class CatController {
+  static async getAllCategories(req: Request, res: Response) {
+    try {
+      const users = await Category.findAll();
+      res.status(200).json(users);
+    } catch(err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  static async getCatById(req: Request, res: Response) {
+    try {
+      const catId = req.params.id;
+      const category = await Category.findByPk(catId);
+
+      if(!category) {
+        res.status(404).json({ error: 'Category not found' });
+      }
+      res.status(200).json(category);
+    } catch(err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
   static async createCategory(req: Request, res: Response) {
     try {
       const { name, is_featured } = req.body;
