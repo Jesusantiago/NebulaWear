98402b046c157f85d948c8172f31ecf1ef5deaec
Viper292 <crazymelon@hotmail.es>
2024-04-23 05:19:10 -0300

    Adds routing
    Additionally fixes unnecessary ID in users router

--- server/src/router/category.router.ts
+++ server/src/router/category.router.ts
@@ -3,4 +3,15 @@ import CatController from '../controllers/category.controller';
 
 const router: Router = express.Router();
 
+// GET /categories
+router.get('/', CatController.getAllCategories);
+// GET /categories/:id
+router.get('/:id', CatController.getCatById);
+// POST /categories
+router.post('/', CatController.createCategory);
+// PATCH /categories/:id
+router.patch('/:id', CatController.updateCategory);
+// DELETE /categories/:id
+router.delete('/:id', CatController.deleteCategory);
+
 export default router;
