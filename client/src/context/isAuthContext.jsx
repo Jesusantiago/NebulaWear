14ac965be98b9f9c311b703d010448f5548360b9
Jesus Santiago <jesusantiago420@gmail.com>
2024-05-13 14:49:46 -0300

    ADD loginWithGoogle button

--- client/src/context/isAuthContext.jsx
+++ client/src/context/isAuthContext.jsx
@@ -35,7 +35,6 @@ export const AuthProvider = ({ children }) => {
     @funtion apiRegister registrar el usuario en nuestra base de datos
 
   */
-  
   const register = async (email, password, name) => {
     try {
       const { user } = await createUserWithEmailAndPassword(auth, email, password)
@@ -61,11 +60,14 @@ export const AuthProvider = ({ children }) => {
     }
   }
 
+  /*
+    @funtion { login } realiza el login con los datos a Firebase, lo guarda en el estado y en el localStorage
+    @funtion signInWithEmailAndPassword funcion de Firebase para logear.
+  */
   const login = async (email, password) => {
     console.log(userCurrent)
     try{
       const { user } = await signInWithEmailAndPassword(auth, email, password);
-      if(user){
         localStorage.setItem("user", user.uid)
         setUserCurrent({
           uid: user.uid,
@@ -73,19 +75,29 @@ export const AuthProvider = ({ children }) => {
           email: user.email,
         })
         console.log(userCurrent)
-      }
     }catch (err){
       console.log(err.message)
     }
   }
 
+  /*
+    @funtion { loginWithGoogle } funcion que logea al usuario, lo guarda en el estado y lo guarda en el localStorage
+    @funtion GoogleAuthProvider() funcion de firebase
+
+  */
   const loginWithGoogle = async () => {
     const responseGoogle = new GoogleAuthProvider()
-    const user = signInWithPopup(auth, responseGoogle)
-    console.log(user)
-
+    const { user } = await signInWithPopup(auth, responseGoogle)
+    setUserCurrent({
+      uid : user.uid,
+      name : user.displayName, 
+    })
+    localStorage.setItem("user" , user.uid)
   }
 
+  /* 
+    @funtion { logout } - funcion que borra el estado, borra el localStorage y cierra sesión con Firebase
+  */
   const logout = async () => {
 
     localStorage.clear()
