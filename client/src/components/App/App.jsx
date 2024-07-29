d73677da05b40acee1a28d061d116074c7da0380
Jesus Santiago <jesusantiago420@gmail.com>
2024-07-29 13:06:39 +0200

    DELETE commit

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -2,6 +2,28 @@ import Navbar from './Navbar';
 import Product  from "../Product/Product";
 import { useAuth } from "../../context/isAuthContext";
 
+const SearchBox = () => {
+  return (
+    <div className="searchBox">
+      {/* <SearchIcon /> */}
+      <input type="text" placeholder="Search product" />
+    </div>
+  );
+}
+
+const Categories = () => {
+  return (
+    <div className="categories">
+      <span className="active">All</span>
+      <span>Clothes</span>
+      <span>Electronics</span>
+      <span>Furniture</span>
+      <span>Toys</span>
+    </div>
+  )
+}
+
+
 function App() {
   const auth = useAuth()
 
@@ -14,7 +36,19 @@ function App() {
     <>
       <Navbar />
       <main>
-
+        <SearchBox />
+        <Categories />
+        
+        <section className="main">
+          <div className="filter">
+            <span>Order:</span>
+            {/* <select name="select">
+              <option value="value1" selected>Most recent</option>
+              <option value="value2">Value 2</option>
+              <option value="value3">Value 3</option>
+            </select> */}
+          </div>
+        <button type='button' onClick={logoutComponent} > Cerrar sesion</button>
 
 
           <div className="productList">
@@ -25,8 +59,8 @@ function App() {
             <Product />
             <Product />
           </div>
+        </section>
 
-          <button type='button' onClick={logoutComponent} > Cerrar sesion</button>
         
       </main>
     </>
