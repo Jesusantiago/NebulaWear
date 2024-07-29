c2c2f4c000ad6648c447480392fd07a815e0b8ee
Jesus Santiago <jesusantiago420@gmail.com>
2024-07-29 18:41:45 +0200

    ADD Search import in Navbar

--- client/src/components/App/Navbar.jsx
+++ client/src/components/App/Navbar.jsx
@@ -12,6 +12,7 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import PersonIcon from '@mui/icons-material/Person2';
+import Search from './Search';
 const pages = ['Productos', 'Precios', 'Ofertas'];
 const settings = ['Mi perfil', 'Mis compras', 'Mis favoritos', 'Cerrar sesión'];
 
